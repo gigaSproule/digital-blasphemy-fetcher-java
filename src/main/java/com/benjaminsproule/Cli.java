992764137e3d0e3b93b36b6ea2e5b2a0f56ff0fb
Benjamin <dev@benjaminsproule.com>
2026-06-04 00:40:34 +0100

    Converted to Java

---
+++ src/main/java/com/benjaminsproule/Cli.java
@@ -0,0 +1,89 @@
+package com.benjaminsproule;
+
+import com.benjaminsproule.digitalblasphemy.client.model.WallpaperType;
+import org.apache.commons.cli.*;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class Cli {
+    private final int width;
+    private final int height;
+    private final WallpaperType wallpaperType;
+    private final Path wallpaperPath;
+
+    public Cli(int width, int height, WallpaperType wallpaperType, Path wallpaperPath) {
+        this.width = width;
+        this.height = height;
+        this.wallpaperType = wallpaperType;
+        this.wallpaperPath = wallpaperPath;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
+
+    public WallpaperType wallpaperType() {
+        return wallpaperType;
+    }
+
+    public Path wallpaperPath() {
+        return wallpaperPath;
+    }
+
+    public static Cli parse(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        Options options = getOptions();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            int width = ((Number) cmd.getParsedOptionValue("width")).intValue();
+            int height = ((Number) cmd.getParsedOptionValue("height")).intValue();
+            WallpaperType type = (WallpaperType) cmd.getParsedOptionValue("type");
+            Path path = (Path) cmd.getParsedOptionValue("path");
+            return new Cli(width, height, type, path);
+        } catch (ParseException e) {
+            formatter.printHelp("digital-blasphemy-fetcher", options);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Options getOptions() {
+        Options opts = new Options();
+        opts.addOption(Option.builder("w")
+                .longOpt("width")
+                .hasArg()
+                .desc("Required width of wallpapers")
+                .required()
+                .type(Integer.class)
+                .get());
+
+        opts.addOption(Option.builder("h")
+                .longOpt("height")
+                .hasArg()
+                .desc("Required height of wallpapers")
+                .required()
+                .type(Integer.class)
+                .get());
+
+        opts.addOption(Option.builder("t")
+                .longOpt("type")
+                .hasArg()
+                .desc("Required type of wallpapers. Valid values are " + Arrays.toString(WallpaperType.values()))
+                .required()
+                .type(WallpaperType.class)
+                .get());
+
+        opts.addOption(Option.builder("p")
+                .longOpt("path")
+                .hasArg()
+                .desc("Path to put wallpapers")
+                .required()
+                .type(Path.class)
+                .get());
+        return opts;
+    }
+}
